dc28e9d715b52513aae84b536757a28ca6334069
sarra <sarra.hammami456@gmail.com>
2023-12-26 01:20:26 +0100

    added the signin code

--- middleware/src/main/java/org/eclipse/jakarta/hello/entities/FieldPropertyVisibilityStrategy.java
+++ middleware/src/main/java/org/eclipse/jakarta/hello/entities/FieldPropertyVisibilityStrategy.java
@@ -3,7 +3,6 @@ package org.eclipse.jakarta.hello.entities;
 import jakarta.json.bind.config.PropertyVisibilityStrategy;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-
 public class FieldPropertyVisibilityStrategy implements PropertyVisibilityStrategy {
 
     @Override
